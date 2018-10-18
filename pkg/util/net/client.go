aca76be265e65b76def53a0abfc19a69ee177c9e
Eric Suen <suen@squareup.com>
2018-10-18 16:40:06 -0700

    test

--- pkg/util/net/client.go
+++ pkg/util/net/client.go
@@ -1,6 +1,7 @@
 package net
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -16,6 +17,8 @@ func (ht headerTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	for k, v := range ht.extras {
 		r.Header.Set(k, v)
 	}
+	fmt.Println("Inside headerTransport RoundTrip")
+	fmt.Printf("ht.extras: %s\n", ht.extras)
 	return ht.inner.RoundTrip(r)
 }
 
